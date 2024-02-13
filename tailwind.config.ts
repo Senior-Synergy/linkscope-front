ff93e0e7d87baacd2cd8ccdbc26c665cf6ef49f2
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-02-13 09:49:34 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,51 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#446CCF",
+        "primary-light": "#718fdb",
+        "primary-dark": "#24438e",
+        "primary-faded": "#c2cfef",
+
+        accent: "#CFA744",
+        "accent-light": "#e5cf9a",
+        "accent-dark": "#8e7024",
+
+        link: "#6144CF",
+        alert: "#CF446C",
+        success: "#66BB6A",
+        deactive: "#D2D2D2",
+
+        background: "#EBECED",
+        text: "#515151",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        moveUpDown: {
+          "0%, 100%": {
+            transform: "translate(0)",
+          },
+          "50%": {
+            transform: "translate(0, 30%)",
+          },
+        },
+        moveLeftRight: {
+          "0%, 100%": {
+            transform: "translate(0)",
+          },
+          "50%": {
+            transform: "translate(-150%, 0)",
+          },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+        moveUpDown: "moveUpDown 2s ease-in-out infinite",
+        moveLeftRight: "moveLeftRight 4s ease-in-out infinite",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
