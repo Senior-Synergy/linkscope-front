0a14c44e6cbe1d08d11934d8d28be0c26a6a77b2
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-02-15 00:54:13 +0700

    Temporarily disable sidebar and update navbar design

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -9,10 +9,6 @@ function Navbar() {
       route: "/",
     },
     {
-      name: "Scan",
-      route: "/scan",
-    },
-    {
       name: "Search",
       route: "/search",
     },
@@ -29,14 +25,15 @@ function Navbar() {
                 flex justify-between items-center 
                 px-4 md:px-8 h-14 z-10
                 shadow-lg
-                bg-white
+                bg-primary-dark
+                border-b-4 border-white
                 "
       >
         {/* Title */}
         <div className="flex items-center md:pl-2">
           <div className="flex flex-col flex-shrink-0">
             <Image
-              src={"/logo/logo-black.svg"}
+              src={"/logo/logo-white.svg"}
               alt="logo"
               width={128}
               height={32}
@@ -45,14 +42,14 @@ function Navbar() {
         </div>
 
         {/* Buttons */}
-        <div className="hidden md:flex h-full">
+        <div className="hidden md:flex items-center gap-4 h-full">
           {NavbarItem.map((item, index) => (
             <Link key={index} href={item.route}>
               <div
                 className="flex items-center justify-center 
-                        w-28 h-full
-                        text-black
-                        border-y-4 border-transparent hover:border-b-primary hover:bg-background hover:bg-opacity-50
+                        w-28 h-10 rounded
+                        text-white font-bold
+                        border-y-4 border-transparent
                         transition-all"
               >
                 {item.name}
@@ -60,9 +57,25 @@ function Navbar() {
             </Link>
           ))}
         </div>
+
+        {/* <div className="flex w-full justify-center bg-white divide-x-2 border-y-4">
+          {NavbarItem.map((item, index) => (
+            <Link key={index} href={item.route}>
+              <div
+                className="flex items-center justify-center 
+                        w-28 h-8
+                        font-bold text-gray-900
+                        border-y-4 border-transparent hover:bg-primary hover:text-white
+                        transition-all"
+              >
+                {item.name}
+              </div>
+            </Link>
+          ))}
+        </div> */}
       </nav>
 
-      <Sidebar navItems={NavbarItem} />
+      {/* <Sidebar navItems={NavbarItem} /> */}
     </>
   );
 }
