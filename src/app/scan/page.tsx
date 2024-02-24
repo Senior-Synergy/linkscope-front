4a6cbfcab40ba40256d6185d8461410332c9b6bc
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-02-24 16:10:54 +0700

    Revert container style to the one without rounded outer corners

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -44,28 +44,27 @@ function Home() {
   ];
 
   return (
-    <div className="flex flex-col flex-auto items-center gap-4 md:gap-8 p-4 md:p-8">
-      <div className="flex flex-col md:flex-row items-center w-full max-w-6xl gap-8 p-8 bg-gradient-to-br from-primary to-primary-dark rounded-2xl">
-        <header className="w-full md:w-1/3 space-y-4 text-white">
-          <h1 className="text-5xl font-bold">LinkScope URL Scanner</h1>
-          <div>
-            <h2 className="text-xl font-semibold mb-2">How to Use:</h2>
-            <ol className="list-decimal pl-6 font-light">
-              <li>{`Enter your text in the input area.`}</li>
-              <li>{`Select the URLs you want to process.`}</li>
-              <li>{`Click the "Submit" button to initiate the scanning process.`}</li>
-            </ol>
-          </div>
-          <p>Follow the steps to scan and process URLs.</p>
-        </header>
-
-        <div className="w-full md:w-2/3">
-          <Scanner />
-        </div>
-      </div>
-
-      <div className="flex flex-col flex-auto gap-8 p-8 self-center w-full max-w-6xl bg-white rounded-2xl">
+    <>
+      <div className="flex flex-col flex-auto gap-8 p-8 self-center w-full max-w-6xl bg-white">
         {/* URL Scanner */}
+        <div className="flex flex-col md:flex-row items-center w-full max-w-6xl gap-8 p-8 bg-gradient-to-br from-primary to-primary-dark rounded-2xl">
+          <header className="w-full md:w-1/3 space-y-4 text-white">
+            <h1 className="text-5xl font-bold">LinkScope URL Scanner</h1>
+            <div>
+              <h2 className="text-xl font-semibold mb-2">How to Use:</h2>
+              <ol className="list-decimal pl-6 font-light">
+                <li>{`Enter your text in the input area.`}</li>
+                <li>{`Select the URLs you want to process.`}</li>
+                <li>{`Click the "Submit" button to initiate the scanning process.`}</li>
+              </ol>
+            </div>
+            <p>Follow the steps to scan and process URLs.</p>
+          </header>
+
+          <div className="w-full md:w-2/3">
+            <Scanner />
+          </div>
+        </div>
 
         <div>
           <h2 className="text-xl font-bold mb-4">What is LinkScope?</h2>
@@ -127,13 +126,13 @@ function Home() {
               <div className="shrink-0 h-6 w-6 rounded-full bg-white" />
             </div>
           </div>
+
+          <footer className="mt-8 text-gray-500 text-sm">
+            <p>LinkScope | Senior Synergy</p>
+          </footer>
         </div>
       </div>
-
-      <footer className="mt-8 text-gray-500 text-sm">
-        <p>LinkScope | Senior Synergy</p>
-      </footer>
-    </div>
+    </>
   );
 }
 
