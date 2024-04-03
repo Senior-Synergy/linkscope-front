cb273f18163377b27c02cb66ee098adf22777a36
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-04-03 23:00:16 +0700

    Change modal behavior

--- src/components/common/ModalWrapper.tsx
+++ src/components/common/ModalWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Portal from "./Portal";
 
 interface ReusableModalProps {
@@ -10,12 +10,33 @@ interface ReusableModalProps {
 }
 
 function ModalWrapper({ isOpen, onClose, children }: ReusableModalProps) {
-  if (!isOpen) return null;
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  });
 
   return (
     <Portal>
-      <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-300 bg-opacity-50 backdrop-blur-sm">
-        <div className="bg-white p-4 rounded-lg max-w-screen-lg w-full overflow-y-auto m-8">
+      <div
+        className={`fixed inset-0 flex items-center justify-center z-50 bg-gray-300 bg-opacity-50 backdrop-blur-sm transition-all
+                  ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+      >
+        <div
+          ref={modalRef}
+          className="bg-white p-4 rounded-lg max-w-screen-lg w-full overflow-y-auto m-8"
+        >
           {children}
           {/* <button
             onClick={onClose}
