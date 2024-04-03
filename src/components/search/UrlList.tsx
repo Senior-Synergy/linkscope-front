5b2c5ee16aa79f30d8983d28ad245542d3b299a2
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-04-03 23:01:29 +0700

    Update UrlList stylings

--- src/components/search/UrlList.tsx
+++ src/components/search/UrlList.tsx
@@ -1,28 +1,39 @@
+import { Url } from "@/types/urlTypes";
 import UrlListItem from "./UrlListItem";
 
-import { UrlInfo, Verdict } from "@/types/searchTypes";
-
 interface UrlListProps {
-  urlList: UrlInfo[];
+  urlList: Url[];
 }
 
-function UrlList({ urlList: searchResults }: UrlListProps) {
+function UrlList({ urlList }: UrlListProps) {
   return (
-    <div
-      className="flex flex-col
+    <div className="space-y-2 p-2 bg-gray-50 rounded-xl border">
+      <div className="flex justify-between p-4 bg-gray-200 border font-bold rounded-lg">
+        <div className="flex">
+          <p>URL</p>
+        </div>
+
+        <div className="flex gap-4 text-center">
+          <p className="hidden sm:block w-20">Date</p>
+          <p className="w-20">Status</p>
+        </div>
+      </div>
+      <div
+        className="flex flex-col
                 w-full 
-                rounded-xl bg-gray-50 border
+                rounded-lg bg-white border
                 divide-y divide
                 overflow-hidden"
-    >
-      {searchResults.map((result, index) => (
-        <UrlListItem
-          key={index}
-          url={result.url}
-          date={result.date}
-          isSafe={result.verdict === Verdict.SAFE}
-        />
-      ))}
+      >
+        {urlList.map((url, index) => (
+          <UrlListItem
+            key={index}
+            url={url.finalURL}
+            date={url.dateUpdated}
+            isSafe={!url.latestResult.isPhish}
+          />
+        ))}
+      </div>
     </div>
   );
 }
