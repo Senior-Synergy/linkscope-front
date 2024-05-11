6b077a4949b99fba92618cd0e602ad7a04b1df5f
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-05-11 20:34:33 +0700

    Fix build issue with useSearchParams and Suspense

--- src/app/url/page.tsx
+++ src/app/url/page.tsx
@@ -1,9 +1,9 @@
 import Footer from "@/components/common/Footer";
 import MainWrapper from "@/components/common/wrapper/MainWrapper";
 import ResultListPaginator from "@/components/search/ResultListPaginator";
-import React from "react";
+import React, { Suspense } from "react";
 
-function UrlPage() {
+async function UrlPage() {
   return (
     <MainWrapper>
       <header>
@@ -13,24 +13,26 @@ function UrlPage() {
         </p>
       </header>
 
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
-        {Array(12)
-          .fill(0)
-          .map((_, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-between gap-4 p-4 h-20 min-w-72 min-h-36 border rounded-lg"
-            >
-              <div className="flex flex-col">
-                <div className="h-4 w-56 bg-gray-300 rounded-full animate-pulse" />
-                <div className="h-4 w-32 mt-2 bg-gray-200 rounded-full animate-pulse" />
-              </div>
-            </div>
-          ))}
-      </section>
-
       <div className="mx-auto mt-4">
-        <ResultListPaginator pageSize={12} totalCount={12} />
+        <Suspense fallback={<UrlGridFallback />}>
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
+            {Array(12)
+              .fill(0)
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className="flex items-center justify-between gap-4 p-4 h-20 min-w-72 min-h-36 border rounded-lg"
+                >
+                  <div className="flex flex-col">
+                    <div className="h-4 w-56 bg-gray-300 rounded-full animate-pulse" />
+                    <div className="h-4 w-32 mt-2 bg-gray-200 rounded-full animate-pulse" />
+                  </div>
+                </div>
+              ))}
+          </section>
+
+          <ResultListPaginator pageSize={12} totalCount={12} />
+        </Suspense>
       </div>
 
       <Footer />
@@ -39,3 +41,23 @@ function UrlPage() {
 }
 
 export default UrlPage;
+
+function UrlGridFallback() {
+  return (
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
+      {Array(12)
+        .fill(0)
+        .map((_, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between gap-4 p-4 h-20 min-w-72 min-h-36 border rounded-lg"
+          >
+            <div className="flex flex-col">
+              <div className="h-4 w-56 bg-gray-300 rounded-full animate-pulse" />
+              <div className="h-4 w-32 mt-2 bg-gray-200 rounded-full animate-pulse" />
+            </div>
+          </div>
+        ))}
+    </section>
+  );
+}
